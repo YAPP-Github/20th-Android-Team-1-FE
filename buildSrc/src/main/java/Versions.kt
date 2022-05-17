d6a4540dec6a4532ec1976142407aa9a15e8fbff
KwonDae <green201402317@gmail.com>
2022-05-17 14:48:25 +0900

    [ISSUE-004] Kotlin&Compose version 수정

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
     const val androidBuildToolVersion = "7.2.0"
     const val androidCoreVersion = "1.7.0"
-    const val kotlinVersion = "1.6.10"
-    const val composeVersion = "1.1.1"
+    const val kotlinVersion = "1.6.20"
+    const val composeVersion = "1.2.0-alpha08"
     const val activityComposeVersion = "1.4.0"
     const val androidAppCompatVersion = "1.4.1"
     const val constraintlayoutVersion = "2.1.3"
