9039bd3000fd4e2cbc636aad547f213d48a75b80
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-05-28 22:13:26 +0900

    feat - BaseViewModel 생성

---
+++ presentation/src/main/java/com/yapp/growth/base/BaseViewModel.kt
@@ -0,0 +1,51 @@
+package com.yapp.growth.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<S : ViewState, A : ViewSideEffect, E : ViewEvent>(
+    initialState: S
+) : ViewModel() {
+
+    abstract fun handleEvents(event: E)
+
+    private val _viewState: MutableStateFlow<S> = MutableStateFlow<S>(initialState)
+    val viewState = _viewState.asStateFlow()
+
+    private val currentState: S
+        get() = _viewState.value
+
+    private val _effect: Channel<A> = Channel()
+    val effect = _effect.receiveAsFlow()
+
+    private val _event: MutableSharedFlow<E> = MutableSharedFlow()
+
+    init {
+        subscribeToEvents()
+    }
+
+    private fun subscribeToEvents() {
+        viewModelScope.launch {
+            _event.collect {
+                handleEvents(it)
+            }
+        }
+    }
+
+    protected fun setState(reducer: S.() -> S) {
+        val newState = currentState.reducer()
+        _viewState.value = newState
+    }
+
+    protected fun setEffect(builder: () -> A) {
+        val effectValue = builder()
+        viewModelScope.launch { _effect.send(effectValue) }
+    }
+
+    open fun setEvent(event : E) {
+        viewModelScope.launch { _event.emit(event) }
+    }
+}
