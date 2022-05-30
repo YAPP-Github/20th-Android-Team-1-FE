0f6d7774bfa9134d0b1506cdaf4f5954f0c2e5d6
jayden.d <jayden.d@kakaocorp.com>
2022-05-30 11:04:59 +0900

    feat - DataModule 추가

--- app/src/main/java/com/yapp/growth/di/DataModule.kt
+++ app/src/main/java/com/yapp/growth/di/DataModule.kt
@@ -1,10 +1,30 @@
 package com.yapp.growth.di
 
+import com.yapp.growth.BuildConfig
+import com.yapp.growth.data.api.NetworkSettings
+import com.yapp.growth.data.api.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object DataModule {
+
+    @Provides
+    @Named("GrowthBaseUrl")
+    fun provideGrowthBaseUrl(): String = BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideNetworkSettings(): NetworkSettings {
+        return NetworkSettings(
+            connectTimeoutMs = 1000 * 10,
+            readTimeoutMs = 1000 * 10,
+            isDebugMode = BuildConfig.DEBUG
+        )
+    }
 }
