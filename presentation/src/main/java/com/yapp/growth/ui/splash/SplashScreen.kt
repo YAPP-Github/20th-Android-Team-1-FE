aaa0d65cf6aa41fa37638c8456efb70c9e616996
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-06-16 19:17:30 +0900

    feat - Splash UI 구성

--- presentation/src/main/java/com/yapp/growth/ui/splash/SplashScreen.kt
+++ presentation/src/main/java/com/yapp/growth/ui/splash/SplashScreen.kt
@@ -8,19 +8,47 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yapp.growth.presentation.R
+import com.yapp.growth.presentation.theme.MainGradient
 import com.yapp.growth.presentation.theme.PlanzTheme
 
 @Composable
 fun SplashScreen() {
-
+    Box(
+        modifier = Modifier
+            .background(brush = MainGradient)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                tint = Color.Unspecified,
+                imageVector = ImageVector.vectorResource(R.drawable.ic_splash_logo),
+                contentDescription = null
+            )
+            Spacer(Modifier.height(18.dp))
+            // TODO : 폰트 관련 디자이너님 컨펌 나오면 수정 필요할수도?
+            Text(
+                text = stringResource(id = R.string.splash_app_name),
+                color = Color.White,
+                style = MaterialTheme.typography.h1
+            )
+            Spacer(Modifier.height(9.dp))
+            Text(
+                text = stringResource(id = R.string.splash_introduce),
+                color = Color.White,
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
 }
 
 @Preview(widthDp = 360, heightDp = 640)
