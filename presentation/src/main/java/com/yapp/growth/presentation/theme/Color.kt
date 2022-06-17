f068b01080cc5aa2994ffec24e511516d7adac26
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-06-17 13:43:53 +0900

    design - 그라데이션 컬러값 조정

--- presentation/src/main/java/com/yapp/growth/presentation/theme/Color.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/theme/Color.kt
@@ -10,8 +10,9 @@ val SubYellow       = Color(0xFFFEDB78)
 val SubCoral        = Color(0XFFFF7F77)
 val MainGradient    = Brush.linearGradient(
                             colors  = listOf(Color(0xFF8886FF), Color(0xFF6671F6)),
-                            start   = Offset.Zero,
+                            start   = Offset(90F, 320F),
                             end     = Offset.Infinite)
+
 
 // TODO :: 디자이너분께서 색상 관련 자세한 정보 주시면 추후 수정
 val Gray900         = Color(0XFF020202)
