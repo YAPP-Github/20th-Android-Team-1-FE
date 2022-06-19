20c017a8f372698639ca3524f8b141ca0ac70f7a
Jihee.Han <42907876+jihee-dev@users.noreply.github.com>
2022-06-20 02:52:25 +0900

    feat - 플랜즈 버튼 컴포넌트 추가

---
+++ presentation/src/main/java/com/yapp/growth/presentation/component/PlanzButtons.kt
@@ -0,0 +1,161 @@
+package com.yapp.growth.presentation.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.yapp.growth.presentation.theme.*
+
+@Composable
+fun PlanzMainButton(
+    text: String,
+    enabled: Boolean = true,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    PlanzButton(
+        text = text,
+        enabled = enabled,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        onClick = onClick,
+    )
+}
+
+@Composable
+fun PlanzSecondButton(
+    text: String,
+    enabled: Boolean = true,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    PlanzButton(
+        text = text,
+        enabled = enabled,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        buttonColor = MainPurpleLight,
+        textColor = MainPurple,
+        onClick = onClick,
+    )
+}
+
+@Composable
+private fun PlanzButton(
+    text: String,
+    enabled: Boolean = true,
+    modifier: Modifier = Modifier,
+    buttonColor: Color = MainPurple,
+    textColor: Color = Gray100,
+    onClick: () -> Unit,
+) {
+    Button(
+        modifier = modifier.height(52.dp),
+        shape = RoundedCornerShape(10.dp),
+        onClick = { if (enabled) onClick() },
+        colors = when (enabled) {
+            true -> ButtonDefaults.buttonColors(backgroundColor = buttonColor)
+            else -> ButtonDefaults.buttonColors(backgroundColor = Gray300)
+        },
+        elevation = null,
+    ) {
+        Text(
+            text = text,
+            color = if (enabled) textColor else Gray500,
+            style = PlanzTypography.button
+        )
+    }
+}
+
+@Composable
+fun PlanzButtonWithBack(
+    text: String,
+    enabled: Boolean = true,
+    modifier: Modifier,
+    onClick: () -> Unit,
+    onBackClick: () -> Unit,
+) {
+    Row(modifier = modifier.padding(horizontal = 16.dp)) {
+        Button(
+            modifier = modifier
+                .width(52.dp)
+                .height(52.dp),
+            shape = RoundedCornerShape(10.dp),
+            onClick = { onBackClick() },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Gray200),
+            elevation = null,
+        ) {
+            Icon(
+                painter = painterResource(id = com.yapp.growth.presentation.R.drawable.ic_arrow_back),
+                contentDescription = "back arrow",
+                tint = Color.Unspecified,
+            )
+        }
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        PlanzButton(
+            text = text,
+            enabled = enabled,
+            modifier = modifier
+                .fillMaxWidth(),
+            onClick = onClick,
+        )
+    }
+
+}
+
+@Preview
+@Composable
+fun PlanzMainButtonPreview() {
+    PlanzMainButton(
+        text = "메인 버튼",
+        enabled = true,
+        modifier = Modifier,
+        onClick = {}
+    )
+}
+
+@Preview
+@Composable
+fun PlanzSecondButtonPreview() {
+    PlanzSecondButton(
+        text = "세컨드 버튼",
+        enabled = true,
+        modifier = Modifier,
+        onClick = {}
+    )
+}
+
+@Preview
+@Composable
+fun PlanzDisabledButtonPreview() {
+    PlanzMainButton(
+        text = "비활성 버튼",
+        enabled = false,
+        modifier = Modifier,
+        onClick = {}
+    )
+}
+
+@Preview
+@Composable
+fun PlanzWithBackButtonPreview() {
+    PlanzButtonWithBack(
+        text = "다음",
+        enabled = false,
+        modifier = Modifier,
+        onClick = {},
+        onBackClick = {}
+    )
+}
