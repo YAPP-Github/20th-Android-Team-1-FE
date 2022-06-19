4c10803b108a398ab5b657231fb6ab3f18bf7303
KwonDae <green201402317@gmail.com>
2022-06-20 02:10:56 +0900

    feat - KakaoModule 생성

---
+++ app/src/main/java/com/yapp/growth/di/KakaoModule.kt
@@ -0,0 +1,31 @@
+package com.yapp.growth.di
+
+import com.kakao.sdk.auth.AuthApiClient
+import com.kakao.sdk.user.UserApiClient
+import com.yapp.growth.ui.sample.LoginSdk
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object KakaoModule {
+    @Singleton
+    @Provides
+    fun provideUserApiClient(): UserApiClient = UserApiClient.instance
+
+    @Singleton
+    @Provides
+    fun provideAuthApiClient(): AuthApiClient = AuthApiClient.instance
+
+    @Singleton
+    @Provides
+    fun provideKakaoLoginSdk(
+        userApiClient: UserApiClient,
+        authApiClient: AuthApiClient
+    ): LoginSdk {
+        return KakaoLoginSdk(userApiClient, authApiClient)
+    }
+}
