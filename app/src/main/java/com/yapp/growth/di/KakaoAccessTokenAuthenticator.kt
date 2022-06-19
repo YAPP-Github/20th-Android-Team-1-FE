92170b5314e8843f10d9395a5b04611a144b2498
jayden.d <jayden.d@kakaocorp.com>
2022-06-20 02:04:05 +0900

    feat - Authenticator 추가

---
+++ app/src/main/java/com/yapp/growth/di/KakaoAccessTokenAuthenticator.kt
@@ -0,0 +1,38 @@
+package com.yapp.growth.di
+
+import com.yapp.growth.ui.sample.LoginSdk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+import javax.inject.Inject
+
+class KakaoAccessTokenAuthenticator @Inject constructor(
+    private val kakaoLoginSdk: LoginSdk
+) : Authenticator {
+
+    private var refreshable: Boolean = true
+
+    override fun authenticate(route: Route?, response: Response): Request? {
+        if (!refreshable || response.code != 401) {
+            refreshable = true
+            return null
+        }
+
+        val isSuccessRefreshToken = runBlocking {
+            withContext(Dispatchers.Default) {
+                kakaoLoginSdk.refreshToken()
+            }
+        }
+        refreshable = isSuccessRefreshToken
+
+        return if (isSuccessRefreshToken) {
+            response.request.newBuilder().build()
+        } else {
+            null
+        }
+    }
+}
