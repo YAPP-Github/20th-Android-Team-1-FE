c79363112ccc5cb9a5b2945243b0d3ecf4ce4369
KwonDae <green201402317@gmail.com>
2022-06-20 17:09:59 +0900

    feat - LoginSdk 인터페이스에 예외처리 항목 안내 주석 추가

--- presentation/src/main/java/com/yapp/growth/LoginSdk.kt
+++ presentation/src/main/java/com/yapp/growth/LoginSdk.kt
@@ -2,13 +2,27 @@ package com.yapp.growth
 
 import android.content.Context
 import com.yapp.growth.ui.sample.KakaoAccessToken
-import com.yapp.growth.ui.sample.KakaoRefreshToken
 
 interface LoginSdk {
+
+    /**
+     * throws 있는 메소드는
+     * 사용할 때 에러 처리를 해주어야 합니다.
+     * ex) runCatching { }, mapCatching { }
+     *
+     */
+
+    @Throws(AuthException::class)
     suspend fun login(context: Context): KakaoAccessToken
+
+    @Throws(AuthException::class)
     suspend fun logout()
+
+    @Throws(AuthException::class)
     suspend fun getAccessToken(): KakaoAccessToken?
+
     suspend fun isValidAccessToken(): Boolean
-    suspend fun refreshToken(): Pair<KakaoAccessToken, KakaoRefreshToken>
+
+    suspend fun refreshToken(): Boolean
 
 }
