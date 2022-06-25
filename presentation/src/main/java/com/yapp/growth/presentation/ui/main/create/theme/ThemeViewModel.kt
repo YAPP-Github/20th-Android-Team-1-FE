9fbf00f06d07b63026a7866a06c3238aefc97f5f
Jihee.Han <42907876+jihee-dev@users.noreply.github.com>
2022-06-25 19:37:00 +0900

    feat - 약속 잡기 테마 선택 화면 구현

---
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/main/create/theme/ThemeViewModel.kt
@@ -0,0 +1,20 @@
+package com.yapp.growth.presentation.ui.main.create.theme
+
+import com.yapp.growth.base.BaseViewModel
+import com.yapp.growth.presentation.ui.main.create.theme.ThemeContract.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ThemeViewModel @Inject constructor(
+) : BaseViewModel<ThemeViewState, ThemeSideEffect, ThemeEvent>(
+    ThemeViewState()
+) {
+    override fun handleEvents(event: ThemeEvent) {
+        when (event) {
+            is ThemeEvent.ChoosePlanTheme -> updateState { copy(chosenTheme = event.theme) }
+            is ThemeEvent.OnClickNextButton -> sendEffect({ ThemeSideEffect.NavigateToNextScreen })
+            is ThemeEvent.OnClickExitButton -> sendEffect({ ThemeSideEffect.ExitCreateScreen } )
+        }
+    }
+}
