e86bd0ea7231f3f5fa5e9889e2708582ace23c27
Jihee.Han <42907876+jihee-dev@users.noreply.github.com>
2022-06-28 06:22:20 +0900

    feat - 약속 잡기 약속명 및 약속 장소 화면 구현

--- presentation/src/main/java/com/yapp/growth/presentation/ui/main/create/title/TitleContract.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/main/create/title/TitleContract.kt
@@ -1,4 +1,31 @@
 package com.yapp.growth.presentation.ui.main.create.title
 
+import com.yapp.growth.base.ViewEvent
+import com.yapp.growth.base.ViewSideEffect
+import com.yapp.growth.base.ViewState
+import com.yapp.growth.presentation.model.PlanThemeType
+
 class TitleContract {
+    data class TitleViewState(
+        val chosenTheme: PlanThemeType? = null,
+        val title: String = "",
+        val place: String = "",
+        val isError: Boolean = false,
+    ) : ViewState
+
+    sealed class TitleSideEffect : ViewSideEffect {
+        object ExitCreateScreen : TitleSideEffect()
+        object NavigateToNextScreen : TitleSideEffect()
+        object NavigateToPreviousScreen : TitleSideEffect()
+    }
+
+    sealed class TitleEvent : ViewEvent {
+        data class FillInTitle(val title: String) : TitleEvent()
+        data class FillInPlace(val place: String) : TitleEvent()
+        object ClearTitle : TitleEvent()
+        object ClearPlace : TitleEvent()
+        object OnClickExitButton : TitleEvent()
+        object OnClickNextButton : TitleEvent()
+        object OnClickBackButton : TitleEvent()
+    }
 }
--- presentation/src/main/java/com/yapp/growth/presentation/ui/main/create/title/TitleViewModel.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/main/create/title/TitleViewModel.kt
@@ -1,4 +1,50 @@
 package com.yapp.growth.presentation.ui.main.create.title
 
-class TitleViewModel {
+import androidx.lifecycle.SavedStateHandle
+import com.yapp.growth.base.BaseViewModel
+import com.yapp.growth.presentation.model.PlanThemeType
+import com.yapp.growth.presentation.ui.main.KEY_PLAN_THEME_TYPE
+import com.yapp.growth.presentation.ui.main.create.title.TitleContract.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TitleViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+) : BaseViewModel<TitleViewState, TitleSideEffect, TitleEvent>(
+    TitleViewState()
+) {
+    init {
+        val chosenTheme = savedStateHandle.get<String>(KEY_PLAN_THEME_TYPE)
+        chosenTheme?.let { updateState { copy(chosenTheme = PlanThemeType.valueOf(chosenTheme)) } }
+    }
+
+    override fun handleEvents(event: TitleEvent) {
+        when (event) {
+            is TitleEvent.FillInTitle -> reflectUpdatedState(title = event.title)
+            is TitleEvent.FillInPlace -> reflectUpdatedState(place = event.place)
+            is TitleEvent.ClearTitle -> reflectUpdatedState(title = "")
+            is TitleEvent.ClearPlace -> reflectUpdatedState(place = "")
+            is TitleEvent.OnClickExitButton -> sendEffect({ TitleSideEffect.ExitCreateScreen })
+            is TitleEvent.OnClickNextButton -> sendEffect({ TitleSideEffect.NavigateToNextScreen })
+            is TitleEvent.OnClickBackButton -> sendEffect({ TitleSideEffect.NavigateToPreviousScreen })
+        }
+    }
+
+    private fun reflectUpdatedState(
+        title: String = viewState.value.title,
+        place: String = viewState.value.place
+    ) {
+        updateState {
+            copy(
+                title = title,
+                place = place,
+                isError = isOverflowed(title, place)
+            )
+        }
+    }
+
+    private fun isOverflowed(title: String, place: String): Boolean {
+        return (title.length > MAX_LENGTH_TITLE) || (place.length > MAX_LENGTH_PLACE)
+    }
 }
