18b332df7682418815e962c406f180f4fc540675
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-06-29 20:27:46 +0900

    feat - Home 화면 앱바 추가

--- presentation/src/main/java/com/yapp/growth/presentation/ui/main/home/HomeScreen.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/main/home/HomeScreen.kt
@@ -1,12 +1,29 @@
 package com.yapp.growth.presentation.ui.main.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.yapp.growth.presentation.theme.PlanzTheme
+import com.yapp.growth.presentation.R
+import com.yapp.growth.presentation.theme.*
 import com.yapp.growth.presentation.ui.main.home.HomeContract.HomeSideEffect
 import kotlinx.coroutines.flow.collect
 
@@ -33,6 +50,61 @@ fun HomeScreen(
                 }
             }
         }
+    }
+
+    Scaffold(
+        backgroundColor = BackgroundColor1,
+        topBar = {
+            HomeAppBar(
+                userName = "김정호",
+                onUserIconClick = { /* TODO */ }
+            )
+        },
+        modifier = Modifier.fillMaxSize(),
+    ) {
+    }
+}
+
+// TODO : 클릭 시 내 정보 화면으로 네비게이션 (정호)
+@Composable
+private fun HomeAppBar(
+    modifier: Modifier = Modifier,
+    userName: String,
+    onUserIconClick: () -> Unit,
+) {
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(60.dp)
+    ) {
+        val (userImage, nameText) = createRefs()
+
+        IconButton(
+            modifier = Modifier
+                .size(30.dp, 30.dp)
+                .constrainAs(userImage) {
+                    start.linkTo(parent.start, margin = 22.dp)
+                    top.linkTo(parent.top, margin = 14.dp)
+                },
+            onClick = { onUserIconClick() }) {
+            Image(
+                painter = painterResource(R.drawable.ic_default_user_image),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .border(0.79.dp, MainPurple900, CircleShape),
+                contentDescription = null,
+            )
+        }
+        Text(
+            modifier = Modifier.constrainAs(nameText) {
+                start.linkTo(userImage.end, margin = 12.dp)
+                top.linkTo(parent.top, margin = 16.dp)
+            },
+            text = userName,
+            style = PlanzTypography.h3,
+            color = Gray900,
+        )
     }
 }
 
