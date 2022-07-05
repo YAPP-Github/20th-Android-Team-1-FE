74360ecd282460e42ac66cb193f3a753982913e2
Jihee.Han <42907876+jihee-dev@users.noreply.github.com>
2022-07-05 22:23:23 +0900

    refactor - 바텀 시트 컴포넌트 변경
    - BottomSheetScaffold -> ModalBottomSheetLayout

---
+++ presentation/src/main/java/com/yapp/growth/presentation/component/PlanzBottomSheet.kt
@@ -0,0 +1,63 @@
+package com.yapp.growth.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetDefaults
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun PlanzBottomSheetLayout(
+    sheetState: ModalBottomSheetState,
+    sheetContent: @Composable () -> Unit,
+    scrimColor: Color = ModalBottomSheetDefaults.scrimColor,
+    content: @Composable () -> Unit,
+) {
+    ModalBottomSheetLayout(
+        sheetBackgroundColor = Color.Transparent,
+        sheetState = sheetState,
+        sheetContent = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+            ) {
+                PlanzBottomSheet {
+                    sheetContent()
+                }
+            }
+        },
+        scrimColor = scrimColor,
+        content = content,
+    )
+}
+
+@Composable
+fun PlanzBottomSheet(
+    sheetContent: @Composable () -> Unit,
+) {
+    Spacer(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(20.dp)
+            .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
+            .background(color = Color.White)
+    )
+
+    sheetContent()
+
+    Spacer(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(20.dp)
+            .background(color = Color.White)
+    )
+}
