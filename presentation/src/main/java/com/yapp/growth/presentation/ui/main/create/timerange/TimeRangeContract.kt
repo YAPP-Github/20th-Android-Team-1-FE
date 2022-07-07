c864f5e17389409ec692ab6a0cea06065b8997b3
Jihee.Han <42907876+jihee-dev@users.noreply.github.com>
2022-07-08 02:58:05 +0900

    feat - 약속 생성 시간대 선택 구현

---
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/main/create/timerange/TimeRangeContract.kt
@@ -0,0 +1,43 @@
+package com.yapp.growth.presentation.ui.main.create.timerange
+
+import com.yapp.growth.base.ViewEvent
+import com.yapp.growth.base.ViewSideEffect
+import com.yapp.growth.base.ViewState
+import com.yapp.growth.presentation.model.PlanThemeType
+
+class TimeRangeContract {
+    data class TimeRangeViewState(
+        val chosenTheme: PlanThemeType? = null,
+        val title: String = "",
+        val place: String = "",
+        val dates: String = "",
+        val startHour: Int? = null,
+        val endHour: Int? = null,
+        val dialogState: DialogState = DialogState.NONE,
+        val isError: Boolean = true,
+    ) : ViewState {
+        enum class DialogState {
+            START_HOUR,
+            END_HOUR,
+            NONE,
+        }
+    }
+
+    sealed class TimeRangeSideEffect : ViewSideEffect {
+        object ExitCreateScreen : TimeRangeSideEffect()
+        object NavigateToNextScreen : TimeRangeSideEffect()
+        object NavigateToPreviousScreen : TimeRangeSideEffect()
+        object ShowBottomSheet : TimeRangeSideEffect()
+        object HideBottomSheet : TimeRangeSideEffect()
+        object ShowSnackBar : TimeRangeSideEffect()
+    }
+
+    sealed class TimeRangeEvent : ViewEvent {
+        object OnClickExitButton : TimeRangeEvent()
+        object OnClickNextButton : TimeRangeEvent()
+        object OnClickBackButton : TimeRangeEvent()
+        object OnStartHourClicked : TimeRangeEvent()
+        object OnEndHourClicked : TimeRangeEvent()
+        data class OnSelectedHourChanged(val hour: Int) : TimeRangeEvent()
+    }
+}
---
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/main/create/timerange/TimeRangeScreen.kt
@@ -0,0 +1,308 @@
+package com.yapp.growth.presentation.ui.main.create.timerange
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.yapp.growth.presentation.R
+import com.yapp.growth.presentation.component.PlanzBottomSheetLayout
+import com.yapp.growth.presentation.component.PlanzButtonWithBack
+import com.yapp.growth.presentation.component.PlanzCreateStepTitle
+import com.yapp.growth.presentation.component.PlanzErrorSnackBar
+import com.yapp.growth.presentation.model.PlanThemeType
+import com.yapp.growth.presentation.model.TimeType
+import com.yapp.growth.presentation.theme.*
+import com.yapp.growth.presentation.ui.main.BLANK_VALUE
+import com.yapp.growth.presentation.ui.main.create.timerange.TimeRangeContract.TimeRangeEvent
+import com.yapp.growth.presentation.ui.main.create.timerange.TimeRangeContract.TimeRangeSideEffect
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun TimeRangeScreen(
+    viewModel: TimeRangeViewModel = hiltViewModel(),
+    exitCreateScreen: () -> Unit,
+    navigateToNextScreen: (PlanThemeType, String, String, String, Int, Int) -> Unit,
+    navigateToPreviousScreen: () -> Unit,
+) {
+    val context = LocalContext.current
+    val viewState by viewModel.viewState.collectAsState()
+    val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
+    // TODO: 드래그로 시트가 닫히지 않도록 수정
+    PlanzBottomSheetLayout(
+        sheetState = sheetState,
+        sheetContent = {
+            TimeRangeTimeBottomSheetContent(
+                timeOptions = TimeType.values(),
+                onItemClick = {
+                    viewModel.setEvent(TimeRangeEvent.OnSelectedHourChanged(it.ordinal))
+                }
+            )
+        }
+    ) {
+        Scaffold(
+            scaffoldState = scaffoldState,
+            topBar = {
+                PlanzCreateStepTitle(
+                    currentStep = 4,
+                    title = stringResource(id = R.string.create_plan_time_range_title_text),
+                    onExitClick = { viewModel.setEvent(TimeRangeEvent.OnClickExitButton) }
+                )
+            },
+            snackbarHost = { snackbarHostState ->
+                SnackbarHost(hostState = snackbarHostState) { snackbarData ->
+                    PlanzErrorSnackBar(message = snackbarData.message)
+                }
+            }
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                TimeRangeText(
+                    modifier = Modifier.padding(top = 140.dp),
+                    isError = viewState.isError,
+                    startHour = viewState.startHour,
+                    endHour = viewState.endHour,
+                    onStartHourClick = { viewModel.setEvent(TimeRangeEvent.OnStartHourClicked) },
+                    onEndHourClick = { viewModel.setEvent(TimeRangeEvent.OnEndHourClicked) }
+                )
+
+                PlanzButtonWithBack(
+                    text = stringResource(id = R.string.create_plan_next_button_text),
+                    enabled = !viewState.isError,
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(bottom = 32.dp),
+                    onClick = { viewModel.setEvent(TimeRangeEvent.OnClickNextButton) },
+                    onBackClick = { viewModel.setEvent(TimeRangeEvent.OnClickBackButton) }
+                )
+            }
+        }
+    }
+
+    LaunchedEffect(key1 = viewModel.effect) {
+        viewModel.effect.collect { effect ->
+            when (effect) {
+                is TimeRangeSideEffect.ExitCreateScreen -> {
+                    exitCreateScreen()
+                }
+                is TimeRangeSideEffect.NavigateToNextScreen -> {
+                    viewState.chosenTheme?.let { theme ->
+                        navigateToNextScreen(
+                            theme,
+                            viewState.title.ifBlank { BLANK_VALUE },
+                            viewState.place.ifBlank { BLANK_VALUE },
+                            viewState.dates.ifBlank { BLANK_VALUE },
+                            viewState.startHour ?: 0,
+                            viewState.endHour ?: 24,
+                        )
+                    }
+                }
+                is TimeRangeSideEffect.NavigateToPreviousScreen -> {
+                    navigateToPreviousScreen()
+                }
+                is TimeRangeSideEffect.ShowBottomSheet -> {
+                    coroutineScope.launch {
+                        sheetState.show()
+                    }
+
+                }
+                is TimeRangeSideEffect.HideBottomSheet -> {
+                    coroutineScope.launch {
+                        sheetState.hide()
+                    }
+                }
+                is TimeRangeSideEffect.ShowSnackBar -> {
+                    coroutineScope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar(
+                            context.getString(R.string.create_plan_time_range_error_message)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TimeRangeTimeBottomSheetContent(
+    timeOptions: Array<TimeType>,
+    onItemClick: (TimeType) -> Unit,
+) {
+    // TODO: Number Picker 형태로 수정
+    // TODO: 스크롤 상태 기억하지 않도록 수정(or 시작 시간은 시작 시간대로, 끝 시간은 끝 시간대로 위치 기억)
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(260.dp)
+    ) {
+        items(timeOptions) { timeOption ->
+            TimeRangeTimeBottomSheetItem(
+                timeOption = timeOption,
+                onItemClick = { onItemClick(timeOption) }
+            )
+        }
+    }
+}
+
+@Composable
+fun TimeRangeTimeBottomSheetItem(
+    timeOption: TimeType,
+    onItemClick: () -> Unit,
+) {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(BackgroundColor3)
+            .clickable { onItemClick() }
+            .padding(vertical = 14.dp),
+        text = stringResource(id = timeOption.timeStringResId),
+        style = PlanzTypography.subtitle1,
+        color = Gray900,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+fun TimeRangeText(
+    modifier: Modifier = Modifier,
+    isError: Boolean,
+    startHour: Int?,
+    endHour: Int?,
+    onStartHourClick: () -> Unit,
+    onEndHourClick: () -> Unit,
+) {
+    ConstraintLayout(
+        modifier = modifier
+            .padding(horizontal = 20.dp),
+    ) {
+        val (startHourText, fromText, endHourText, toText, startHourUnderline, endHourUnderline) = createRefs()
+
+        Box(
+            modifier = Modifier
+                .width(50.dp)
+                .height(12.dp)
+                .constrainAs(startHourUnderline) {
+                    start.linkTo(parent.absoluteLeft)
+                    bottom.linkTo(parent.bottom, 4.dp)
+                }
+                .background(
+                    when {
+                        startHour == null -> Gray100
+                        endHour != null && isError -> SubCoral.copy(alpha = 0.4f)
+                        else -> MainPurple200
+                    }
+                )
+        )
+
+        Box(
+            modifier = Modifier
+                .width(50.dp)
+                .height(12.dp)
+                .constrainAs(endHourUnderline) {
+                    start.linkTo(fromText.end, 12.dp)
+                    bottom.linkTo(parent.bottom, 4.dp)
+                }
+                .background(
+                    when {
+                        endHour == null -> Gray100
+                        startHour != null && isError -> SubCoral.copy(alpha = 0.4f)
+                        else -> MainPurple200
+                    }
+                )
+        )
+
+        Text(
+            modifier = Modifier
+                .constrainAs(startHourText) {
+                    start.linkTo(startHourUnderline.absoluteLeft)
+                    end.linkTo(startHourUnderline.absoluteRight)
+                    baseline.linkTo(parent.baseline)
+                }
+                .clickable { onStartHourClick() },
+            text = String.format("%02d", (startHour ?: 0)),
+            style = PlanzTypography.h1.copy(
+                fontSize = 36.sp
+            ),
+            color = when {
+                startHour == null -> Gray300
+                endHour != null && isError -> SubCoral
+                else -> MainPurple900
+            },
+        )
+
+        Text(
+            modifier = Modifier.constrainAs(fromText) {
+                start.linkTo(startHourUnderline.end, 8.dp)
+                baseline.linkTo(parent.baseline)
+            },
+            text = stringResource(id = R.string.create_plan_time_range_from_text),
+            style = PlanzTypography.subtitle1,
+            color = Gray900
+        )
+
+        Text(
+            modifier = Modifier
+                .constrainAs(endHourText) {
+                    start.linkTo(endHourUnderline.start)
+                    end.linkTo(endHourUnderline.end)
+                    baseline.linkTo(parent.baseline)
+                }
+                .clickable { onEndHourClick() },
+            text = String.format("%02d", (endHour ?: 24)),
+            style = PlanzTypography.h1.copy(
+                fontSize = 36.sp
+            ),
+            color = when {
+                endHour == null -> Gray300
+                startHour != null && isError -> SubCoral
+                else -> MainPurple900
+            },
+        )
+
+        Text(
+            modifier = Modifier.constrainAs(toText) {
+                start.linkTo(endHourUnderline.end, 8.dp)
+                baseline.linkTo(parent.baseline)
+            },
+            text = stringResource(id = R.string.create_plan_time_range_to_text),
+            style = PlanzTypography.subtitle1,
+            color = Gray900
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TimeRangeTimeBottomSheetItemPreview() {
+    TimeRangeTimeBottomSheetItem(
+        timeOption = TimeType.AM_01,
+        onItemClick = {}
+    )
+}
+
+@Preview
+@Composable
+fun TimeRangeScreenPreview() {
+    TimeRangeScreen(
+        exitCreateScreen = {},
+        navigateToNextScreen = { theme, title, place, dates, startHour, endHour -> },
+        navigateToPreviousScreen = {}
+    )
+}
