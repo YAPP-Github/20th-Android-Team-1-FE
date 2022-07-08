add8643cb00b3513124e55db8cf330a140482276
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-07-08 15:04:26 +0900

    fix - 누락된 HiltViewModel 어노테이션 추가

--- presentation/src/main/java/com/yapp/growth/presentation/ui/main/detailPlan/DetailPlanViewModel.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/main/detailPlan/DetailPlanViewModel.kt
@@ -2,8 +2,10 @@ package com.yapp.growth.presentation.ui.main.detailPlan
 
 import com.yapp.growth.base.BaseViewModel
 import com.yapp.growth.presentation.ui.main.detailPlan.DetailPlanContract.*
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class DetailPlanViewModel @Inject constructor(
 ) : BaseViewModel<DetailPlanViewState, DetailPlanSideEffect, DetailPlanEvent>(
     DetailPlanViewState
