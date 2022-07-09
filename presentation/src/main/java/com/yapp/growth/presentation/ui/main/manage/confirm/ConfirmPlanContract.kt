71d25dc892786e6aee6f82e77eab87224ea9432c
KwonDae <green201402317@gmail.com>
2022-07-09 17:29:29 +0900

    feat - ConfirmPlanContract 기본코드 작성

--- presentation/src/main/java/com/yapp/growth/presentation/ui/main/manage/confirm/ConfirmPlanContract.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/main/manage/confirm/ConfirmPlanContract.kt
@@ -1,4 +1,22 @@
 package com.yapp.growth.presentation.ui.main.manage.confirm
 
+import com.yapp.growth.base.ViewEvent
+import com.yapp.growth.base.ViewSideEffect
+import com.yapp.growth.base.ViewState
+
 class ConfirmPlanContract {
+    data class ConfirmPlanViewState(
+        val state: Boolean = false
+    ): ViewState
+
+    sealed class ConfirmPlanSideEffect: ViewSideEffect {
+
+    }
+
+    sealed class ConfirmPlanEvent: ViewEvent {
+        object OnClickNextDayButton: ConfirmPlanEvent()
+        object OnClickPreviousDayButton: ConfirmPlanEvent()
+        data class OnClickTimeTable(val dateIndex: Int, val minuteIndex: Int): ConfirmPlanEvent()
+        data class OnClickConfirmButton(val dateIndex: Int, val minuteIndex: Int): ConfirmPlanEvent()
+    }
 }
