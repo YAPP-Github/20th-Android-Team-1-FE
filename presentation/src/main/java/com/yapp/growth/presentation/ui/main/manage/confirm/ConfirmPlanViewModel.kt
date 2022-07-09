e1dd36f4bed66b37f3a2b781663b8031c413d57f
KwonDae <green201402317@gmail.com>
2022-07-09 17:29:16 +0900

    feat - ConfirmPlanViewModel 기본코드 작성

--- presentation/src/main/java/com/yapp/growth/presentation/ui/main/manage/confirm/ConfirmPlanViewModel.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/main/manage/confirm/ConfirmPlanViewModel.kt
@@ -1,4 +1,30 @@
 package com.yapp.growth.presentation.ui.main.manage.confirm
 
-class ConfirmPlanViewModel {
+import com.yapp.growth.base.BaseViewModel
+import com.yapp.growth.domain.entity.RespondPlan
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import com.yapp.growth.presentation.ui.main.manage.confirm.ConfirmPlanContract.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+@HiltViewModel
+class ConfirmPlanViewModel @Inject constructor(
+
+): BaseViewModel<ConfirmPlanViewState, ConfirmPlanSideEffect, ConfirmPlanEvent>(
+    ConfirmPlanViewState()
+) {
+    private val _dates = MutableStateFlow<List<RespondPlan>>(emptyList())
+    val dates: StateFlow<List<RespondPlan>>
+        get() = _dates.asStateFlow()
+
+    override fun handleEvents(event: ConfirmPlanEvent) {
+        when (event) {
+            is ConfirmPlanEvent.OnClickNextDayButton -> TODO()
+            is ConfirmPlanEvent.OnClickPreviousDayButton -> TODO()
+            is ConfirmPlanEvent.OnClickConfirmButton -> TODO()
+            is ConfirmPlanEvent.OnClickTimeTable -> TODO()
+        }
+    }
 }
