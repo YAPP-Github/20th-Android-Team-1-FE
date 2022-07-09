f416f8b5978f4212377be80f14f07e47b470665f
KwonDae <green201402317@gmail.com>
2022-07-09 17:52:01 +0900

    feat - domain 모듈 GetRespondUsersUseCase 추가

--- domain/src/main/java/com/yapp/growth/domain/usecase/GetRespondUsersUseCase.kt
+++ domain/src/main/java/com/yapp/growth/domain/usecase/GetRespondUsersUseCase.kt
@@ -1,11 +1,15 @@
 package com.yapp.growth.domain.usecase
 
+import com.yapp.growth.domain.NetworkResult
+import com.yapp.growth.domain.entity.RespondUsers
 import com.yapp.growth.domain.repository.ConfirmPlanRepository
-import com.yapp.growth.domain.repository.UserRepository
 import javax.inject.Inject
 
-class GetRespondUsersUseCase@Inject constructor(
+class GetRespondUsersUseCase @Inject constructor(
     private val repository: ConfirmPlanRepository
 ) {
+    suspend fun invoke(promisingKey: Long): NetworkResult<List<RespondUsers>> {
+        return repository.getRespondUsers(promisingKey)
+    }
 
 }
