b0cb398e255145ff5ea2d2aaa3a8e8f1f5818bf1
KwonDae <green201402317@gmail.com>
2022-07-09 18:43:24 +0900

    chore - test 파일 네이밍 변경

--- data/src/main/java/com/yapp/growth/data/mapper/ResponseToModelMapper.kt
+++ data/src/main/java/com/yapp/growth/data/mapper/ResponseToModelMapper.kt
@@ -1,10 +1,10 @@
 package com.yapp.growth.data.mapper
 
 import com.yapp.growth.data.response.UserResponse
-import com.yapp.growth.domain.entity.User
+import com.yapp.growth.domain.entity.TestingUser
 
-fun mapUserDto(input: UserResponse): User {
-    return User(
+fun mapUserDto(input: UserResponse): TestingUser {
+    return TestingUser(
         login = input.login,
         nodeId = input.nodeId,
         avatarUrl = input.avatarUrl,
