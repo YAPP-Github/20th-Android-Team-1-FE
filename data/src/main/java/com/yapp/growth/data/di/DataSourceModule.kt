c237065ad44630552f9e9cefc7ab5801abd715ec
KwonDae <green201402317@gmail.com>
2022-07-09 17:50:01 +0900

    feat - dataSource, repository 의존성 추가

--- data/src/main/java/com/yapp/growth/data/di/DataSourceModule.kt
+++ data/src/main/java/com/yapp/growth/data/di/DataSourceModule.kt
@@ -1,10 +1,11 @@
 package com.yapp.growth.data.di
 
+import com.yapp.growth.data.source.ConfirmPlanDataSource
+import com.yapp.growth.data.source.ConfirmPlanDataSourceImpl
 import com.yapp.growth.data.source.UserDataSource
 import com.yapp.growth.data.source.UserDataSourceImpl
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -16,4 +17,8 @@ internal abstract class DataSourceModule {
     @Binds
     @Singleton
     abstract fun bindUserDataSource(dataSource: UserDataSourceImpl): UserDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindConfirmPlanDataSource(confirmPlanDataSource: ConfirmPlanDataSourceImpl): ConfirmPlanDataSource
 }
