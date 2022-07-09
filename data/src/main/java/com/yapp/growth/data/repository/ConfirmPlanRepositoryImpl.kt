cb50b4b82037b9f8b8db51fd8f9fc894fbb8081a
KwonDae <green201402317@gmail.com>
2022-07-09 17:49:18 +0900

    feat - data 모듈 repositoryImpl 추가

---
+++ data/src/main/java/com/yapp/growth/data/repository/ConfirmPlanRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.yapp.growth.data.repository
+
+import com.yapp.growth.data.source.ConfirmPlanDataSource
+import com.yapp.growth.domain.NetworkResult
+import com.yapp.growth.domain.entity.RespondUsers
+import com.yapp.growth.domain.repository.ConfirmPlanRepository
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+internal class ConfirmPlanRepositoryImpl @Inject constructor(
+    private val dataSource: ConfirmPlanDataSource
+) : ConfirmPlanRepository {
+
+    override suspend fun getRespondUsers(promisingKey: Long): NetworkResult<List<RespondUsers>> {
+        return dataSource.getRespondUsers(promisingKey)
+    }
+
+}
