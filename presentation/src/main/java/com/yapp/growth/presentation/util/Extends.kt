9eff45cc67995243c22004c86397f68cbfe3ba2d
KwonDae <green201402317@gmail.com>
2022-07-10 22:04:21 +0900

    feat - api 통신으로 받은 날짜 변환하는 확장함수 추가

---
+++ presentation/src/main/java/com/yapp/growth/presentation/util/Extends.kt
@@ -0,0 +1,32 @@
+package com.yapp.growth.presentation.util
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+internal val PARSE_DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA)
+
+//fun Date.toDay(): String {
+//    val dfDay: DateFormat = SimpleDateFormat("M/d", Locale.KOREA)
+//    return dfDay.format(this)
+//}
+//
+//fun Date.toHour(): String {
+//    val dfHour: DateFormat = SimpleDateFormat("HH:mm", Locale.KOREA)
+//    return dfHour.format(this)
+//}
+
+fun Date.toCalculateDiffDay(other: Date): Long {
+    return (other.time - this.time) / (60 * 60 * 24 * 1000)
+}
+
+fun String.toDay(): String {
+    val dateTimeFormat = SimpleDateFormat("M/d", Locale.KOREA)
+    val displayText = PARSE_DATE_FORMAT.parse(this) ?: ""
+    return dateTimeFormat.format(displayText)
+}
+
+fun String.toHour(): String {
+    val dateTimeFormat = SimpleDateFormat("HH:mm", Locale.KOREA)
+    val displayText = PARSE_DATE_FORMAT.parse(this) ?: ""
+    return dateTimeFormat.format(displayText)
+}
