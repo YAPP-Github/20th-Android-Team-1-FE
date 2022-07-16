20645ec16b9dc84c470bf67ddac8f5e8c1709840
KwonDae <green201402317@gmail.com>
2022-07-16 16:38:01 +0900

    refactor - 약속확정, 약속응답 약속박스 디자인 수정

--- presentation/src/main/java/com/yapp/growth/presentation/component/PlanzTimeTable.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/component/PlanzTimeTable.kt
@@ -1,5 +1,6 @@
 package com.yapp.growth.presentation.component
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -12,7 +13,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -69,23 +72,47 @@ fun ConfirmPlanTimeTable(
                         block.find { it.index == minuteIndex.plus(1) }?.color ?: 0x00000000
                     } ?:  0x00000000
 
-                    Column {
+                    Column(modifier = Modifier
+                        .wrapContentSize()
+                        .border(
+                            width = 0.5.dp,
+                            color = Gray200,
+                            shape = RectangleShape
+                        )
+                    ) {
                         Box(
                             modifier = Modifier
                                 .height(26.dp)
                                 .fillParentMaxWidth(1f / (responsePlan.availableDate.size + 1))
                                 .border(
-                                    width = 0.5.dp,
-                                    color = Gray200,
+                                    width = if (upperTableClicked) 3.dp else 0.dp,
+                                    color = if (upperTableClicked) SubCoral else Color.Transparent,
                                     shape = RectangleShape
                                 )
                                 .clickable {
                                     onClickTimeTable(dateIndex, minuteIndex)
                                 }
-                                .background(if (upperTableClicked) SubCoral else Color(upperTableColor)),
+                                .background(if (upperTableClicked) SubCoral.copy(0.5f) else Color.Transparent),
                             contentAlignment = Alignment.Center
                         ) {
+                            Box(modifier = Modifier
+                                .height(26.dp)
+                                .fillParentMaxWidth()
+                                .background(Color(upperTableColor))
+                            )
+                        }
 
+                        val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+                        Canvas(Modifier
+                            .fillParentMaxWidth(1f / (responsePlan.availableDate.size + 1))
+                            .height(1.dp)
+                        ) {
+                            drawLine(
+                                color = Gray200,
+                                start = Offset(0f, 0f),
+                                end = Offset(size.width, 0f),
+                                pathEffect = pathEffect
+                            )
                         }
 
                         Box(
@@ -93,17 +120,21 @@ fun ConfirmPlanTimeTable(
                                 .height(26.dp)
                                 .fillParentMaxWidth(1f / (responsePlan.availableDate.size + 1))
                                 .border(
-                                    width = 0.5.dp,
-                                    color = Gray200,
+                                    width = if (underTableClicked) 3.dp else 0.5.dp,
+                                    color = if (underTableClicked) SubCoral else Gray200,
                                     shape = RectangleShape
                                 )
                                 .clickable {
                                     onClickTimeTable(dateIndex, minuteIndex.plus(1))
                                 }
-                                .background(if (underTableClicked) SubCoral else Color(underTableColor)),
+                                .background(if (underTableClicked) SubCoral.copy(0.5f) else Color.Transparent),
                             contentAlignment = Alignment.Center
                         ) {
-
+                            Box(modifier = Modifier
+                                .height(26.dp)
+                                .fillParentMaxWidth()
+                                .background(Color(underTableColor))
+                            )
                         }
                     }
 
@@ -157,24 +188,48 @@ fun PlanzPlanTimeTable(
                         block.find { it.index == minuteIndex.plus(1) }?.color ?: 0x00000000
                     } ?:  0x00000000
 
-                    Column {
+                    Column(modifier = Modifier
+                        .wrapContentSize()
+                        .border(
+                            width = 0.5.dp,
+                            color = Gray200,
+                            shape = RectangleShape
+                        )
+                    ) {
                         Box(
                             modifier = Modifier
                                 .height(26.dp)
                                 .fillParentMaxWidth(1f / (responsePlan.availableDate.size + 1))
                                 .border(
-                                    width = 0.5.dp,
-                                    color = Gray200,
+                                    width = if (upperTableClicked) 3.dp else 0.dp,
+                                    color = if (upperTableClicked) SubCoral else Color.Transparent,
                                     shape = RectangleShape
                                 )
                                 .clickable {
                                     upperTableClicked = !upperTableClicked
                                     onClickTimeTable(dateIndex, minuteIndex)
                                 }
-                                .background(if (upperTableClicked) SubCoral else Color(upperTableColor)),
+                                .background(if (upperTableClicked) SubCoral.copy(0.5f) else Color.Transparent),
                             contentAlignment = Alignment.Center
                         ) {
+                            Box(modifier = Modifier
+                                .height(26.dp)
+                                .fillParentMaxWidth()
+                                .background(Color(upperTableColor))
+                            )
+                        }
 
+                        val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+                        Canvas(Modifier
+                            .fillParentMaxWidth(1f / (responsePlan.availableDate.size + 1))
+                            .height(1.dp)
+                        ) {
+                            drawLine(
+                                color = Gray200,
+                                start = Offset(0f, 0f),
+                                end = Offset(size.width, 0f),
+                                pathEffect = pathEffect
+                            )
                         }
 
                         Box(
@@ -182,18 +237,22 @@ fun PlanzPlanTimeTable(
                                 .height(26.dp)
                                 .fillParentMaxWidth(1f / (responsePlan.availableDate.size + 1))
                                 .border(
-                                    width = 0.5.dp,
-                                    color = Gray200,
+                                    width = if (underTableClicked) 3.dp else 0.dp,
+                                    color = if (underTableClicked) SubCoral else Color.Transparent,
                                     shape = RectangleShape
                                 )
                                 .clickable {
                                     underTableClicked = !underTableClicked
                                     onClickTimeTable(dateIndex, minuteIndex.plus(1))
                                 }
-                                .background(if (underTableClicked) SubCoral else Color(underTableColor)),
+                                .background(if (underTableClicked) SubCoral.copy(0.5f) else Color.Transparent),
                             contentAlignment = Alignment.Center
                         ) {
-
+                            Box(modifier = Modifier
+                                .height(26.dp)
+                                .fillParentMaxWidth()
+                                .background(Color(underTableColor))
+                            )
                         }
                     }
 
