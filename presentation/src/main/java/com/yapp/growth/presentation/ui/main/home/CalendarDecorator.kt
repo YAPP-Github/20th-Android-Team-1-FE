252d892171dc2663250b0878ae91bc9b0f013c89
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-07-18 17:26:23 +0900

    refactor - 캘린더 데코레이터 네이밍 변경

--- presentation/src/main/java/com/yapp/growth/presentation/ui/main/home/CalendarDecorator.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/main/home/CalendarDecorator.kt
@@ -17,19 +17,81 @@ import com.prolificinteractive.materialcalendarview.DayViewFacade
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import com.prolificinteractive.materialcalendarview.spans.DotSpan.DEFAULT_RADIUS
 import com.yapp.growth.presentation.R
-import com.yapp.growth.presentation.theme.*
+import com.yapp.growth.presentation.theme.Gray300
+import com.yapp.growth.presentation.theme.Gray900
+import com.yapp.growth.presentation.theme.MainPurple900
+import com.yapp.growth.presentation.theme.SubCoral
+import com.yapp.growth.presentation.theme.SubYellow
 import java.util.*
 
 
 class CalendarDecorator {
+
+    class TodayLessDecorator(context: Context) : DayViewDecorator {
+        private var date = CalendarDay.today()
+        private val boldSpan = StyleSpan(Typeface.NORMAL)
+
+        val drawable = ContextCompat.getDrawable(context, R.drawable.bg_calendar_disabled_selection)
+
+        @SuppressLint("UseCompatLoadingForDrawables")
+        override fun shouldDecorate(day: CalendarDay): Boolean {
+            return day.isBefore(date)
+        }
+
+        override fun decorate(view: DayViewFacade) {
+            view.apply {
+                this.addSpan(object : ForegroundColorSpan(Gray300.toArgb()) {})
+                this.addSpan(boldSpan)
+                this.setSelectionDrawable(drawable!!)
+            }
+        }
+    }
+
+    class TodayBoldDecorator : DayViewDecorator {
+        private var date = CalendarDay.today()
+        private val boldSpan = StyleSpan(Typeface.BOLD)
+
+        @SuppressLint("UseCompatLoadingForDrawables")
+        override fun shouldDecorate(day: CalendarDay): Boolean {
+            return day == date
+        }
+
+        override fun decorate(view: DayViewFacade) {
+            view.apply {
+                this.addSpan(boldSpan)
+            }
+        }
+    }
+
     class TodayDecorator(context: Context) : DayViewDecorator {
         private var date = CalendarDay.today()
         private val boldSpan = StyleSpan(Typeface.BOLD)
 
         @SuppressLint("UseCompatLoadingForDrawables")
-        val drawable = ContextCompat.getDrawable(context, R.drawable.bg_calendar_today)
+        val drawable = ContextCompat.getDrawable(context, R.drawable.bg_calendar_enabled)
         override fun shouldDecorate(day: CalendarDay): Boolean {
             return day == date
+        }
+
+        override fun decorate(view: DayViewFacade) {
+            if (drawable != null) {
+                view.apply {
+                    this.addSpan(object : ForegroundColorSpan(Color.White.toArgb()) {})
+                    this.setSelectionDrawable(drawable)
+                    this.addSpan(boldSpan)
+                }
+            }
+        }
+    }
+
+    class SelectedDatesDecorator(context: Context, private val selectedDates: List<CalendarDay>) :
+        DayViewDecorator {
+        @SuppressLint("UseCompatLoadingForDrawables")
+        private val drawable = ContextCompat.getDrawable(context, R.drawable.bg_calendar_enabled_selection)
+        private val boldSpan = StyleSpan(Typeface.BOLD)
+
+        override fun shouldDecorate(day: CalendarDay): Boolean {
+            return selectedDates.contains(day)
         }
 
         override fun decorate(view: DayViewFacade) {
@@ -46,7 +108,7 @@ class CalendarDecorator {
     class SelectDecorator(context: Context, mCalendar: MaterialCalendarView) : DayViewDecorator {
         @SuppressLint("UseCompatLoadingForDrawables")
         private val mCalendar = mCalendar.currentDate.calendar
-        private val drawable = ContextCompat.getDrawable(context, R.drawable.bg_calendar_selection)
+        private val drawable = ContextCompat.getDrawable(context, R.drawable.bg_calendar_disabled_selection)
 
         override fun shouldDecorate(day: CalendarDay): Boolean {
             val calendar = day.calendar
@@ -66,10 +128,10 @@ class CalendarDecorator {
         }
     }
 
-    class OtherDayDecorator(context: Context, mCalendar: MaterialCalendarView) : DayViewDecorator {
+    class OtherMonthDisableSelectionDecorator(context: Context, mCalendar: MaterialCalendarView) : DayViewDecorator {
         @SuppressLint("UseCompatLoadingForDrawables")
         private val mCalendar = mCalendar.currentDate.calendar
-        private val drawable = ContextCompat.getDrawable(context, R.drawable.bg_calendar_selection)
+        private val drawable = ContextCompat.getDrawable(context, R.drawable.bg_calendar_disabled_selection)
         override fun shouldDecorate(day: CalendarDay): Boolean {
             val calendar = day.calendar
 
@@ -110,7 +172,16 @@ class CalendarDecorator {
         }
 
         override fun decorate(view: DayViewFacade) {
-            view.addSpan(CustomMultipleDotSpan(4F, color = intArrayOf(SubCoral.toArgb(), SubYellow.toArgb(), MainPurple900.toArgb())))
+            view.addSpan(
+                CustomMultipleDotSpan(
+                    4F,
+                    color = intArrayOf(
+                        SubCoral.toArgb(),
+                        SubYellow.toArgb(),
+                        MainPurple900.toArgb()
+                    )
+                )
+            )
         }
     }
 
@@ -152,14 +223,19 @@ class CalendarDecorator {
             lineNum: Int
         ) {
             val total = if (color.size > 5) 5 else color.size
-            var leftMost = (total - 1) * - 10
+            var leftMost = (total - 1) * -10
 
             for (i in 0 until total) {
                 val oldColor: Int = paint.color
                 if (color[i] != 0) {
                     paint.color = color[i]
                 }
-                canvas.drawCircle(((left + right) / 2 - leftMost).toFloat(), bottom + 10F , radius, paint)
+                canvas.drawCircle(
+                    ((left + right) / 2 - leftMost).toFloat(),
+                    bottom + 10F,
+                    radius,
+                    paint
+                )
                 paint.color = oldColor
                 leftMost += 20
             }
