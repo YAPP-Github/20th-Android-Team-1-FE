3b66f40fcaedf84dec69b6ff7235778a04caa4d8
KwonDae <green201402317@gmail.com>
2022-07-22 20:21:41 +0900

    feat : 약속확정 UseCase 생성

--- domain/src/main/java/com/yapp/growth/domain/usecase/SendConfirmPlanUseCase.kt
+++ domain/src/main/java/com/yapp/growth/domain/usecase/SendConfirmPlanUseCase.kt
@@ -1,15 +1,13 @@
 package com.yapp.growth.domain.usecase
 
 import com.yapp.growth.domain.NetworkResult
-import com.yapp.growth.domain.entity.TimeTable
 import com.yapp.growth.domain.repository.ConfirmPlanRepository
 import javax.inject.Inject
 
-class GetRespondUsersUseCase @Inject constructor(
+class SendConfirmPlanUseCase @Inject constructor(
     private val repository: ConfirmPlanRepository
 ) {
-    suspend operator fun invoke(promisingId: Long): NetworkResult<TimeTable> {
-        return repository.getRespondUsers(promisingId)
+    suspend operator fun invoke(promisingId: Long, date: String): NetworkResult<Any> {
+        return repository.sendConfirmPlan(promisingId, date)
     }
-
 }
