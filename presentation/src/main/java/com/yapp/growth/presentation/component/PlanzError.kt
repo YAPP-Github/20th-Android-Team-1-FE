ff85ea3afca581258fd8db7c7cecfe1e6ea1d5a8
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-07-25 11:12:34 +0900

    feat - 에러 화면을 공통 컴포넌트로 분리

---
+++ presentation/src/main/java/com/yapp/growth/presentation/component/PlanzError.kt
@@ -0,0 +1,62 @@
+package com.yapp.growth.presentation.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.yapp.growth.presentation.R
+import com.yapp.growth.presentation.theme.Gray500
+import com.yapp.growth.presentation.theme.PlanzTypography
+
+
+@Composable
+fun PlanzError(modifier : Modifier) {
+    Box(modifier = modifier) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_failed_character_53),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                text = stringResource(id = R.string.planz_error_text_01),
+                color = Gray500,
+                style = PlanzTypography.body2,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = stringResource(id = R.string.planz_error_text_02),
+                color = Gray500,
+                style = PlanzTypography.body2,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewPlanError() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        PlanzError(modifier = Modifier.fillMaxHeight())
+    }
+}
