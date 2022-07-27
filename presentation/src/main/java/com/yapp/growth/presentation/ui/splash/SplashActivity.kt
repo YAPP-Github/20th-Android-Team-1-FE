6acad8bf18476291cff908b81c57f249a9c1d68d
KwonDae <green201402317@gmail.com>
2022-07-28 02:06:05 +0900

    feat : SplashActivity 에서 다이나믹 링크 수신 구현

--- presentation/src/main/java/com/yapp/growth/presentation/ui/splash/SplashActivity.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/splash/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.yapp.growth.presentation.ui.splash
 
 import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -9,6 +10,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.LaunchedEffect
 import androidx.lifecycle.lifecycleScope
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.ktx.Firebase
+import com.yapp.growth.presentation.firebase.SchemeType
 import com.yapp.growth.presentation.theme.PlanzTheme
 import com.yapp.growth.presentation.ui.login.LoginActivity
 import com.yapp.growth.presentation.ui.main.MainActivity
@@ -16,6 +20,7 @@ import com.yapp.growth.presentation.ui.splash.SplashContract.*
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
@@ -31,6 +36,7 @@ class SplashActivity : ComponentActivity() {
             content.viewTreeObserver.addOnPreDrawListener { false }
         }
 
+        handleDynamicLinks()
         setSplashScreen()
 
         lifecycleScope.launch {
@@ -67,5 +73,34 @@ class SplashActivity : ComponentActivity() {
 
     companion object {
         private const val SPLASH_TIME = 1_000L
+    }
+
+    private fun handleDynamicLinks() {
+        Firebase.dynamicLinks
+            .getDynamicLink(intent)
+            .addOnSuccessListener(this) { pendingDynamicLinkData ->
+                // Get deep link from result (may be null if no link is found)
+                var deepLink: Uri? = null
+                if (pendingDynamicLinkData != null) {
+                    deepLink = pendingDynamicLinkData.link
+                }
+
+                // Handle the deep link. For example, open the linked
+                // content, or apply promotional credit to the user's
+                // account.
+                deepLink?.let {
+                    val segment: String = deepLink.lastPathSegment!!
+                    when(segment) {
+                        SchemeType.RESPOND.name.lowercase() -> {
+                            
+                        }
+                    }
+
+                    MainActivity.dynamicLinkId = deepLink.toString() ?: ""
+                }
+            }
+            .addOnFailureListener(this) {
+                Timber.tag("FAIL HANDLE LINKS").e(it)
+            }
     }
 }
