c141956b7c94082d1f33f3796cbdfe172a3108cf
KwonDae <green201402317@gmail.com>
2022-07-31 20:40:00 +0900

    feat : 다이나믹 링크 썸네일 추가

--- presentation/src/main/java/com/yapp/growth/presentation/ui/createPlan/CreatePlanScreen.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/createPlan/CreatePlanScreen.kt
@@ -2,15 +2,20 @@ package com.yapp.growth.presentation.ui.createPlan
 
 import android.content.Intent
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.yapp.growth.presentation.theme.BackgroundColor1
 import com.yapp.growth.presentation.ui.createPlan.date.DateScreen
 import com.yapp.growth.presentation.ui.createPlan.share.ShareScreen
 import com.yapp.growth.presentation.ui.createPlan.theme.ThemeScreen
@@ -24,6 +29,22 @@ fun CreatePlanScreen(
     exitCreatePlan: () -> Unit,
     startShareActivity: (Intent) -> Unit,
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = MaterialTheme.colors.isLight
+    var statusBarColor: Color by remember { mutableStateOf(Color.White) }
+
+    statusBarColor = when (currentDestination?.route) {
+        CreatePlanScreenRoute.SHARE.route -> {
+            BackgroundColor1
+        }
+        else -> {
+            Color.White
+        }
+    }
+
     Scaffold { innerPadding ->
         NavHost(
             modifier = Modifier.padding(innerPadding),
@@ -91,6 +112,17 @@ fun CreatePlanScreen(
                 )
             }
         }
+    }
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = statusBarColor,
+            darkIcons = useDarkIcons
+        )
+
+        systemUiController.setNavigationBarColor(
+            color = BackgroundColor1
+        )
     }
 }
 
--- presentation/src/main/java/com/yapp/growth/presentation/ui/createPlan/share/ShareViewModel.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/createPlan/share/ShareViewModel.kt
@@ -1,11 +1,13 @@
 package com.yapp.growth.presentation.ui.createPlan.share
 
 import android.content.Context
+import android.net.Uri
 import androidx.lifecycle.SavedStateHandle
 import com.google.firebase.dynamiclinks.ktx.*
 import com.google.firebase.ktx.Firebase
 import com.yapp.growth.base.BaseViewModel
 import com.yapp.growth.presentation.BuildConfig
+import com.yapp.growth.presentation.R
 import com.yapp.growth.presentation.firebase.SchemeType
 import com.yapp.growth.presentation.firebase.getDeepLink
 import com.yapp.growth.presentation.ui.createPlan.share.ShareContract.*
@@ -45,12 +47,23 @@ class ShareViewModel @Inject constructor(
     fun getDynamicLink(
         context: Context,
         scheme: SchemeType = SchemeType.RESPOND,
-        id: String = planId.toString()
+        id: String = planId.toString(),
+        thumbNailTitle: String,
+        thumbNailDescription: String,
+        thumbNailImageUrl: String,
     ) {
         Firebase.dynamicLinks.shortLinkAsync {
             link = getDeepLink(scheme.name, scheme.key, id)
             domainUriPrefix = BuildConfig.PLANZ_FIREBASE_PREFIX
             androidParameters(context.packageName) { }
+            iosParameters(context.packageName) {
+                setFallbackUrl(Uri.parse("https://jalynne.notion.site/3379be16ecc04914bb98f8a57c980a46"))
+            }
+            socialMetaTagParameters {
+                title = thumbNailTitle
+                description = thumbNailDescription
+                imageUrl = Uri.parse(thumbNailImageUrl)
+            }
 
         }.addOnSuccessListener { (shortLink, _) ->
             updateState { copy(shareUrl = shortLink.toString()) }
