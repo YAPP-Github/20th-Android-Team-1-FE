9ff75e0023ee90a62d1b73015751e75033509f3f
KwonDae <green201402317@gmail.com>
2022-07-31 13:58:28 +0900

    fix : 약속 확정 화면 앱바 패딩 수정

--- presentation/src/main/java/com/yapp/growth/presentation/component/PlanzIconAndTextAppBar.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/component/PlanzIconAndTextAppBar.kt
@@ -1,10 +1,7 @@
 package com.yapp.growth.presentation.component
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -55,8 +52,8 @@ private fun PlanzIconAndTextAppBar(
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(76.dp)
-            .padding(top = 20.dp)
+            .wrapContentHeight()
+            .padding(top = 20.dp, bottom = 15.dp)
     ) {
 
         Icon(
