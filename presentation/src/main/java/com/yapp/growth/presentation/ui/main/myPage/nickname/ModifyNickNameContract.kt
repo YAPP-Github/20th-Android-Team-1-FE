9b55254e1070fbffe3cc4aec88e8a91092fefd92
KwonDae <green201402317@gmail.com>
2022-08-02 02:58:25 +0900

    feat : 닉네임 수정 로직 구현

--- presentation/src/main/java/com/yapp/growth/presentation/ui/main/myPage/nickname/ModifyNickNameContract.kt
+++ presentation/src/main/java/com/yapp/growth/presentation/ui/main/myPage/nickname/ModifyNickNameContract.kt
@@ -1,4 +1,25 @@
 package com.yapp.growth.presentation.ui.main.myPage.nickname
 
+import com.yapp.growth.base.LoadState
+import com.yapp.growth.base.ViewEvent
+import com.yapp.growth.base.ViewSideEffect
+import com.yapp.growth.base.ViewState
+
 class ModifyNickNameContract {
+    data class ModifyViewState(
+        val loadState: LoadState = LoadState.SUCCESS,
+        val nickName: String = "",
+        val nickNameHint: String = "",
+        val isError: Boolean = true,
+    ) : ViewState
+
+    sealed class ModifyNickNameSideEffect : ViewSideEffect {
+        object NavigateToPreviousScreen : ModifyNickNameSideEffect()
+    }
+
+    sealed class ModifyNickNameEvent : ViewEvent {
+        data class FillInNickName(val nickName: String) : ModifyNickNameEvent()
+        object ClearNickName : ModifyNickNameEvent()
+        object OnClickModifyButton : ModifyNickNameEvent()
+    }
 }
